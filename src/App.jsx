95af90571bd1d51703b521133fddb7a6dde262a8
Tornado780 <jimmygarg4959@gmail.com>
2025-07-05 17:55:39 +0530

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React, { use } from 'react'
 import Navbar from './components/Navbar'
 import { useLocation, Routes,Route } from 'react-router-dom'
 import Home from './pages/home'
+import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms'
+import RoomDetails from './pages/RoomDetails'
+import MyBookings from './pages/MyBookings'
 function App() {
    const isOwnerPath= useLocation().pathname.includes("owner");
   return (
@@ -10,10 +14,16 @@ function App() {
       <div className='min-h-[70vh]'>
            <Routes>
               <Route path='/' element={<Home />}/>
+              <Route path='/rooms' element={<AllRooms/>}/>
+              <Route path='/rooms/:id' element={<RoomDetails/>}></Route>
+              <Route path='/my-bookings' element={<MyBookings/>}></Route>
           </Routes>
       </div>
+      <Footer/>
     </div>
+    
   )
+ 
 }
 
 export default App
