28e3d38f30cbfed930d1a1abb369ed6aeccbf48c
Tornado780 <jimmygarg4959@gmail.com>
2025-07-08 10:53:27 +0530

    backend structure added

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+
+connectDB();
+const app= express();
+const PORT=process.env.PORT || 5000;
+app.use(cors());
+
+
+app.use(express.json());
+app.use(clerkMiddleware());
+
+
+app.get("/", (req, res) => {
+    res.send("Hello from server");
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
