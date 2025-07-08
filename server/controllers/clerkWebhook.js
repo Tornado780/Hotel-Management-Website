a8e248cab44a8069561bdedd1e8e1729193287b5
Tornado780 <jimmygarg4959@gmail.com>
2025-07-08 12:58:49 +0530

    backend deployed for testing

---
+++ server/controllers/clerkWebhook.js
@@ -0,0 +1,46 @@
+import User from "../models/User.js";
+import { Webhook } from "svix";
+const clerkWebhooks = async (req, res)=>{
+    try {
+           // Create a Svix instance with clerk webhook secret.
+            const whook = new Webhook(process.env.CLERK_WEBHOOK_KEY);
+            // Getting Headers
+            const headers = {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers ["svix-timestamp"],
+            "svix-signature": req.headers ["svix-signature"],
+            };
+            // Verify the payload
+            const payload = await whook.verify(req.JSON.stringify(req.body), headers);
+            const { data,type } = payload;
+            const userData ={
+                _id: data.id,
+                username: data.first_name + " " + data.last_name,
+                email: data.email_addresses[0].email,
+                image: data.image_url,
+            }
+
+            switch(type){
+                case "user.created":
+                    await User.create(userData);
+                    break;
+                case "user.updated":
+                    await User.findByIdAndUpdate(data.id, userData)
+                    break;
+                case "user.deleted":
+                    await User.findOneAndDelete(data.id );
+                    break;
+
+                default:
+                    break;
+                
+            }
+            res.json({success: true, msg: "webhook verified"});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, msg: "webhook not verified"});
+    }
+}
+
+export default clerkWebhooks
